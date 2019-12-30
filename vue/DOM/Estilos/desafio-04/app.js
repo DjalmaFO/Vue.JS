446a069f2ee99f4438b2eec55739d1bc25774c27
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2019-12-30 16:09:51 -0300

    1912301609

--- vue/DOM/Estilos/desafio-04/app.js
+++ vue/DOM/Estilos/desafio-04/app.js
@@ -1,18 +1,42 @@
 new Vue({
-	el: '#desafio',
-	data: {
-		aplicarEfeito: false
-	},
-	methods: {
-		iniciarEfeito() {
-			return {
-				encolher: this.aplicarEfeito,
-				destaque: !this.aplicarEfeito
-			}
+    el: '#desafio',
+    data: {
+        aplicarEfeito: 'destaque',
+        perigo: true,
+        classeCSS: '',
+        ativar: '',
+        cor: '',
+        estilo: {
+            width: '200px',
+            height: '200px'
+        },
+        width: '0'
+    },
+    methods: {
+        iniciarEfeito() {
+            setInterval(() => {
+                    this.aplicarEfeito = this.aplicarEfeito == 'destaque' ?
+                        'encolher' :
+                        'destaque'
+                },
+                1000)
 
-		},
-		iniciarProgresso() {
+        },
+        iniciarProgresso() {
+            let valor = 0
+            const temporizador = setInterval(() => {
+                valor += 4
+                this.width = valor + '%' //`${valor}%`
+                if (valor == 100) clearInterval(temporizador) // para reiniciar => valor = 0
+            }, 500)
 
-		}
-	}
+        },
+        setPerigo(event) {
+            if (event.target.value == 'liga') {
+                this.perigo = true
+            } else if (event.target.value == 'desliga') {
+                this.perigo = false
+            }
+        }
+    }
 })
